545bfe7c21cc8a50f3d9ddde942931e3b9c91c3c
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-17 22:22:22 +0530

    Added Approach 1

--- day-01/rearrange_positive_negative_numbers_alternatively.go
+++ day-01/rearrange_positive_negative_numbers_alternatively.go
@@ -1,3 +1,40 @@
+// Approach 1 :  Using Two Arrays
+
+// Original array : {1, 2, -3, 4, -5}
+
+// Iterate through the numbers 
+
+// if number is negative , add in negativeArray
+// else add in positiveArray
+
+// append both arrays
+
+// Result Array : {-3 , -5 ,1, 2, 4}
+
+func rearrangeArray_using_two_arrays(arr []int) {
+	var negativeArray = make([]int, 0, 0)
+	var positiveArray = make([]int, 0, 0)
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < 0 {
+			negativeArray = append(negativeArray, arr[i])
+		}
+
+		if arr[i] > 0 {
+			positiveArray = append(positiveArray, arr[i])
+		}
+	}
+	j := 0
+	for i := len(negativeArray); i < len(arr); i++ {
+		negativeArray = append(negativeArray, positiveArray[j])
+		j++
+	}
+	for i := 0; i < len(arr); i++ {
+		arr[i] = negativeArray[i]
+	}
+
+}
+
+
 // Approach 2 :  Using Insertion Sort
 
 // Original array : {1, 2, -3, 4, -5}
