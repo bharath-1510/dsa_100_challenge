2245b7670c21eaa9bfc79c8d2633dc3cba2abec7
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-17 22:19:56 +0530

    Added 2nd Approach

--- day-01/day01_test.go
+++ day-01/day01_test.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"testing"
 )
-
+// Rearrange the Postive and Negative Number using insertion sort
 func TestRearrangeArrayUsingInsertionSort(t *testing.T) {
 	tests := []struct {
 		input []int
@@ -22,6 +22,28 @@ func TestRearrangeArrayUsingInsertionSort(t *testing.T) {
 		rearrangeArray_using_insertion_sort(arr)
 		if !reflect.DeepEqual(arr, tc.want) {
 			t.Errorf("rearrangeArray_using_insertion_sort(%v) = %v; want %v", tc.input, arr, tc.want)
+		}
+	}
+}
+
+// Rearrange the Postive and Negative Number using Two Arrays
+func TestRearrangeArrayUsingTwoArrays(t *testing.T) {
+	tests := []struct {
+		input []int
+		want  []int
+	}{
+		{input: []int{1, 2, -3, 4, -5}, want: []int{-3, -5, 1, 2, 4}},
+		{input: []int{12, 11, -13, -5, 6, -7, 5, -3, -6}, want: []int{-13, -5, -7, -3, -6, 12, 11, 6, 5}},
+		{input: []int{1}, want: []int{1}},
+		{input: []int{}, want: []int{}},
+	}
+
+	for _, tc := range tests {
+		arr := make([]int, len(tc.input))
+		copy(arr, tc.input)
+		rearrangeArray_using_two_arrays(arr)
+		if !reflect.DeepEqual(arr, tc.want) {
+			t.Errorf("rearrangeArray_using_two_arrays(%v) = %v; want %v", tc.input, arr, tc.want)
 		}
 	}
 }
