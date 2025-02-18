c1f9e37389234b365506ea629fe1fa68576be11c
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-19 01:38:00 +0530

    Added Testcase for 2nd Problem

--- day-02/day02_test.go
+++ day-02/day02_test.go
@@ -25,3 +25,49 @@ func TestStockBuySell(t *testing.T) {
 		}
 	}
 }
+
+func TestFindFirstandLastOccurence(t *testing.T) {
+	tests := []struct {
+		input    []int
+		target   int
+		expected []int
+	}{
+		{[]int{2, 2, 3, 3, 4, 5, 5}, 2, []int{0, 1}},
+		{[]int{2, 2, 3, 3, 4, 4, 5}, 2, []int{0, 1}},
+		{[]int{2, 3, 3, 4, 4, 5, 5}, 3, []int{1, 2}},
+		{[]int{2, 2, 3, 4, 4, 5, 5}, 4, []int{3, 4}},
+		{[]int{2, 2, 3, 3, 4, 5, 5, 6, 6}, 5, []int{5, 6}},
+	}
+
+	for _, test := range tests {
+		first, last := FindFirstandLastOccurence(test.input, test.target)
+		result := []int{first, last}
+		if !equalSlices(result, test.expected) {
+			t.Errorf("For input %v, expected %v, but got %v", test.input, test.expected, result)
+		}
+	}
+
+}
+
+func TestFindFirstandLastOccurenceUsingBinarySearch(t *testing.T) {
+	tests := []struct {
+		input    []int
+		target   int
+		expected []int
+	}{
+		{[]int{2, 2, 3, 3, 4, 5, 5}, 2, []int{0, 1}},
+		{[]int{2, 2, 3, 3, 4, 4, 5}, 2, []int{0, 1}},
+		{[]int{2, 3, 3, 4, 4, 5, 5}, 3, []int{1, 2}},
+		{[]int{2, 2, 3, 4, 4, 5, 5}, 4, []int{3, 4}},
+		{[]int{2, 2, 3, 3, 4, 5, 5, 6, 6}, 5, []int{5, 6}},
+	}
+
+	for _, test := range tests {
+		first, last := FindFirstandLastOccurenceUsingBinarySearch(test.input, test.target)
+		result := []int{first, last}
+		if !equalSlices(result, test.expected) {
+			t.Errorf("For input %v, expected %v, but got %v", test.input, test.expected, result)
+		}
+	}
+
+}
