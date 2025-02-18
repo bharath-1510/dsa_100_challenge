da43987cfa138c5a9d77634d7f306e4cc8298bd1
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-19 02:04:11 +0530

    Added 2nd Approach

--- day-02/find_first_last_occurence_in_given_array.go
+++ day-02/find_first_last_occurence_in_given_array.go
@@ -14,3 +14,42 @@ func FindFirstandLastOccurence(arr []int, target int) (int, int) {
 	}
 	return first, last
 }
+
+// Approach : Using Binary Search
+func FindFirstandLastOccurenceUsingBinarySearch(arr []int, target int) (int, int) {
+	
+	first := -1
+	low := 0
+	high := len(arr) - 1
+	// Searching for First Occurence
+	for low <= high {
+		mid := low + (high-low)/2
+		if arr[mid] == target {
+			first = mid
+			high = mid - 1
+		} else if arr[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+
+	}
+	last := -1
+	low = 0
+	high = len(arr) - 1
+	// Searching for Last Occurence
+	for low <= high {
+		mid := low + (high-low)/2
+		if arr[mid] == target {
+			last = mid
+			low = mid + 1
+		} else if arr[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+
+	}
+	return first, last
+
+}
