655a95d7973857d5afc32c153f5901b9dbc46145
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-20 02:26:25 +0530

    Solution added for 3rd Problem

---
+++ day-03/missing_number.go
@@ -0,0 +1,46 @@
+// Brute force
+func FindMissingNumber(arr []int) int {
+
+	for i := 1; i <= len(arr)+1; i++ {
+		flag := false
+		for _, val := range arr {
+			if val == i {
+				flag = true
+				break
+			}
+		}
+		if !flag {
+			return i
+		}
+	}
+	return 0
+}
+
+// Using Formula n*(n-1)/2
+func FindMissingNumberUsingFormula(arr []int) int {
+	n := len(arr) + 2
+	sum := 0
+	for i := 0; i < len(arr); i++ {
+		if i < len(arr)-1 && arr[i] == arr[i+1] {
+			n--
+			continue
+		}
+		sum = sum + arr[i]
+	}
+	res := n * (n - 1) / 2
+	return res - sum
+}
+
+//Using Map
+func FindMissingNumberUsingHash(arr []int) int {
+	flag := make(map[int]bool)
+	for _, val := range arr {
+		flag[val] = true
+	}
+	for i := 1; i <= len(arr)+1; i++ {
+		if !flag[i] {
+			return i
+		}
+	}
+	return 0
+}
