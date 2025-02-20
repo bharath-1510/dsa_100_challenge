2555fa8d68baee3271d89fcccba7d71cc69a85a1
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-21 00:52:23 +0530

    Solution Added

---
+++ day-04/reverse_array.go
@@ -0,0 +1,25 @@
+// Approach 1 : Using Auxillary Array
+func reverseArray_using_aux_array(arr []int) {
+	res := make([]int, len(arr))
+	start := 0
+	for i := len(arr) - 1; i >= 0; i-- {
+		res[start] = arr[i]
+		start++
+	}
+	for i := 0; i < len(arr); i++ {
+		arr[i] = res[i]
+	}
+}
+
+// Approach 2 :  Using Two Pointer
+func reverseArrayTwoPointer(arr []int) {
+	start := 0
+	end := len(arr) - 1
+	for start <= end {
+		temp := arr[start]
+		arr[start] = arr[end]
+		arr[end] = temp
+		start++
+		end--
+	}
+}
