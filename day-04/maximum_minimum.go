16012f0d4ea80c5a8b3aef1483b6b230a6502773
Bharath <89100001+bharath-1510@users.noreply.github.com>
2025-02-21 01:00:11 +0530

    Solution Added

---
+++ day-04/maximum_minimum.go
@@ -0,0 +1,15 @@
+func FindMaximumAndMinimum(arr []int) (int, int) {
+	max := math.MinInt
+	min := math.MaxInt
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < min {
+			min = arr[i]
+		}
+	}
+	return min, max
+}
